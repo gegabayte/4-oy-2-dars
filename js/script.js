ff569a92a8573c5e8954a6417a494150d0800468
gegabayte <xasanboymuhammaddinov41@gmail.com>
2024-01-13 23:02:48 +0500

    chenged

---
+++ js/script.js
@@ -0,0 +1,53 @@
+//1-misol 
+// let num = 50;
+// if (num > 0) {
+//     console.log(musbat);
+// } else {
+//     console.log(manfiy);
+// }
+
+
+// 2-misol
+// let number = 50;
+// if (number >= 90 && number <= 100) {
+//     console.log('A');
+// } else if (number >= 80 && number <=89) {
+//     console.log('B');
+// } else if (number >= 70 && number <=79) {
+//     console.log('C');
+// } else if (number >= 60 && number <=69) {
+//     console.log('D');
+// } else if (number >= 0 && number <=59) {
+//     console.log('F');
+// }
+
+
+//3-misol
+// let year = 2028;
+// if ((year % 4 == 0 && year % 100 !=0) || year % 400 == 0) {
+//     console.log(`kabisa yili`);
+// } else {
+//     console.log(`kabisa yil emas`);
+// }
+
+
+//4-misol 
+// let number = 50;
+// if (0 >= number) {
+//     console.log(manfiy);
+// } else {
+//     console.log(musbat);
+// }
+
+
+//5-misol
+// let firstNumber = +prompt(`birinchi ixtiyoriy sonni kiriting`);
+// let secondNumber = +prompt(`ikkinchi ixtiyoriy sonni kiriting`);
+// if (firstNumber > secondNumber ) {
+//     console.log(katta);
+// } else {
+//     console.log(kichkina);
+// }
+
+
+//6-misol
